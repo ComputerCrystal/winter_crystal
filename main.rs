76de3c9415b8448a1dc3c779930036ad29748803
ComputerCrystal <95935625+ComputerCrystal@users.noreply.github.com>
2022-03-29 19:23:12 -0600

    Add files via upload
    A very simple dice game about aespa

--- main.rs
+++ main.rs
@@ -1,64 +1,203 @@
-struct Funcion1elemento {
-    variable: char,
-    coeficiente: i64,
-    exponente: i64,
-}
-impl Funcion1elemento {
-    fn comprobar_derivada(&self) -> bool {
-        self.variable > 'k'
-    }
-}
-
-struct Funcion2elementos {
-    variable: char,
-    coeficiente: i64,
-    exponente: i64,
-    coeficiente2: i64,
-    exponente_general: i64
-}
-
-struct Derivada(i64,char,i64);
+use std::io;
+use rand::Rng;
 
 fn main() {
-    let cambio: (i64,char,i64);
-    let user1 = Funcion1elemento {
-        variable: 'x',
-        coeficiente: 5,
-        exponente: 10,
+    println!("\n\tDICESPA GAME");
+    println!("
+    ------------------------------------------------------
+    |    ....       .       :::.       .::::     .::::.  |
+    | .:   .:.  --.   - :+:.  ...  :-:.  .:-  -:.  .:-.  |
+    |       =:+-     *.:-       ::.       -+:        +.  |
+    |  :=: -%+    :+-           . .      .*   :+:   +=   |
+    | :=-   .+-  :==.         ..  -+      =+ .++.    +-  |
+    |-=.    -== .-.           .*  -+     -+.:*:      +-  |
+    |*     := ::      -.      +-  :=    ==  -=      ---  |
+    |.:   ::   ...  .:.     .+:   :-  .=:    :.   .:.  . |
+    |                             :-                     |
+    |                             :-                     |
+    |                             :-                     |
+    |____________________________________________________|");
+    let dice: u8 = rand::thread_rng().gen_range(1..7);
+
+    println!("\n\tPlease pulse enter to play!");
+    let mut play = String::new();
+
+    io::stdin()
+            .read_line(&mut play)
+            .expect("Failed to read line");
+
+    println!("
+        -----
+        |   |
+        | o |
+        |   |
+        ----- "
+    );
+    println!("\n\tResultado:");
+    match dice {
+        2 => add_symbol(2),
+        5 => add_cover(5),
+        _other => random(1),
     };
-    let user2 = Funcion2elementos {
-        variable: 'x',
-        coeficiente: 5,
-        exponente: 10,
-        coeficiente2: 4,
-        exponente_general: 8,
+}
+
+fn add_symbol(_x: u8) {
+    println!("
+        -----
+        |o  |
+        |   |
+        |  o|
+        ----- " );
+    let symbol: u8 = rand::thread_rng().gen_range(1..5);
+    match symbol {
+        1 => {
+            println!("\n\tWinter");
+            println!("
+             ==
+            -%%:
+           .%@@#.
+    .------#@@@@*--::-:.
+     -*%%@@@@@@@@@@%#*-
+       .=#%@@@@@@%#=.
+        .*%%%@@%%%+
+        +#%%####%%#-
+       =*#*=:  :+*#*.
+       +=:       .-++ ");
+        },
+        2 => {
+            println!("\n\tKarina");
+            println!("
+      .===-.    .-=+=.
+     -#####*=..=*#####-
+    :*#*****#++#*****#*:
+    .=++++++++++++++++=.
+     .-==============-.
+      .:------------:.
+        ::---------:.
+         .:------:.
+           .:--:.
+             :: ");
+        },
+        3 => {
+            println!("\n\tGiselle");
+            println!("
+              .:=+-.
+                 -#%+.
+                  :%@#=
+                   #@@#-
+                  .#@@%*
+    -             *%@@%#
+    -+:         -#%@@%#-
+     =##+=---=*#%%%@%#=
+      .+#%%%%%%%%%%#+:
+        .-+*####*+-. "
+            );
+    },
+        _other => {
+            println!("\n\tNingning");
+            println!("
+        ..            ..
+        -+*+:      .=*+=
+         =***=    =***+.
+         =****=  -****=
+         .+***+..=***+.
+         :+***+:.+***+:
+         .=+++-  -++++.
+          .-=-.   -==: ");
+        },
     };
-
-     if user1.comprobar_derivada() {
-         cambio = derivada_algebraica(&user1);
-         let derivada = Derivada(cambio.0,cambio.1,cambio.2);
-         println!("La derivada es {}{}^{}",derivada.0,derivada.1,derivada.2);
-         derivada_raiz_cuadrada(&user1,&derivada);
-         derivada_compuesta(&user2,&derivada);
-     }
-     else {
-         println!("La derivada es 0");
-     }
 }
 
-fn derivada_algebraica(funcion: &Funcion1elemento) -> (i64,char,i64){
-    (funcion.coeficiente*funcion.exponente,funcion.variable, funcion.exponente -1)
+fn add_cover(_x: u8) {
+    println!("
+        -----
+        |o o|
+        | o |
+        |o o|
+        -----");
+    println!("
+    @@@@@@@@@@@@@@@@%%%%%%%%@%%%%%%%%%%%%%%%%%%%%%%%%%
+    @@@@@@@@@@@@@@@@@%@%@@@%%%@@@@@@@%%@%%%%%%%%%%%%%%
+    @@@@@@@@@@@@@@@@@@%@#=-=*##*+==-::.=@%%%%%%%%%%%%%
+    @@@@@@@@@@@@@@@@@%@*    :=.....::. -@%%%%%%%%%%%%%
+    @@@@@@@@@@@@@@%%%%@-      .-*#%%-: *@%%%%%%%%%%%%%
+    @@@@@@@@@@@%%%%%@@@+ -=-.  :@@@=: :%%%%%%%%%%%%%%%
+    @@@@@@@@%%%%%@@@%*=. :--   .%#=::.%%%%%%%%%%%%%%%%
+    @@@%%%%%%%%@@%*-.  :: ==.:... .: +%%%%%%%%%%%%%%%%
+    %%%%%%%%%%@*:...-*#%%#*=.. .  . -%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%@*:.::---::.         -%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%@@##+=--.          .#%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%*:         =%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%+         *%%%%%%%%%%%%%%#%%%%
+    %%%%%%%%%%%%%%%%%%%%-        =%#%#################
+    %%%%%%%%%%%%%%%%%%%+        :%####################
+    %%%%%%%%%%%%%%%%%%-         -%####################
+    %%%%%%%%%%%%%%%%%:          .#####################
+    %%%%%%%%%%%%%##%-            *####################
+    %%%%%%%%%%####%=             =%###################
+    %%%%%%%%#####%=              :####################
+    %%%%%%########*+..            *###################
+    %%%%%#########%#:... ::       +###################
+    %%%%############:::..*#*- ..:=+###################
+    %%%############%-:-::*###=.:===###################
+    %%%%%%%%%%%%###%=.---####%=.:--+%#################
+    %%%%%%%%%%%%%%%%+.--=%%%##%=.:--+%%###############
+    %%%%%%%%%%%%%%%#:.::+%%%%%%%+.:--+#%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%*    *%%%%%%%%*:..  *%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%*   .#%%%%%%%#%#:   .*%###%%%%%%%%%
+    %%%%%%%%######%+   :############.   .#############
+    ###############=   -##########*#*.   -#***********
+    ###############=   +###########*#*.   *#**********
+    ###############+  .*#************#*.  -#**********
+    ###############+  :#****************.  +**********
+    ########******#+  :#****************=  .**********
+    **************+.  :******************.  =*********
+    *************-     +********+*******+   :*********
+    ***********++:    :+************++++=   .+********
+    ************+=:::-=+++++++++*******+=:::-+++++++**
+    ************+++====++++++++++++=======++++******++
+    ");
 }
 
-fn derivada_raiz_cuadrada(funcion: &Funcion1elemento, derivada: &Derivada) {
-    println!("\nLa derivada es:\n {}{}^{}\n--------\n2√{}{}^{}",derivada.0,derivada.1,derivada.2,
-    funcion.coeficiente,funcion.variable,funcion.exponente);
-}
-
-fn derivada_compuesta(funcion: &Funcion2elementos, derivada: &Derivada){
-    println!("\nLa funcion es: ({}{}^{}+{})^{}",funcion.coeficiente,funcion.variable,funcion.exponente,funcion.coeficiente2,
-    funcion.exponente_general);
-    println!("\nLa derivada es: {}{}^{}({}{}^{}+{})^{}",funcion.exponente_general*derivada.0,funcion.variable,funcion.exponente-1,
-    funcion.coeficiente,funcion.variable,funcion.exponente,funcion.coeficiente2,
-    funcion.exponente_general-1);
+fn random(_x: u8) {
+    let song_number: u8 = rand::thread_rng().gen_range(1..5);
+    match song_number {
+        1 => {
+            println!("
+        -----
+        |   |
+        | o |
+        |   |
+        -----" );
+            println!("\n\tSong: Next Level");
+        }
+        2 => {
+            println!("
+        -----
+        |o  |
+        | o |
+        |  o|
+        -----" );
+            println!("\n\tSong: Yeppi Yeppi");
+        }
+        3 => {
+            println!("
+        -----
+        |o o|
+        |   |
+        |o o|
+        -----" );
+            println!("\n\tSong: Black Mamba");
+        }
+        4 => {
+            println!("
+        -----
+        |o o|
+        |o o|
+        |o o|
+        -----" );
+            println!("\n\tSong: Savage");
+        }
+        _other => (),
+    }
 }
